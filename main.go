485f8ddcaf659b8795b86de14a2c84b7f94cfd45
Christian Svensson <blue@cmd.nu>
2020-12-03 19:28:09 +0100

    configuration file

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 
 	"github.com/coreos/go-systemd/daemon"
 	"github.com/prometheus/client_golang/prometheus"
@@ -14,10 +16,12 @@ import (
 	"github.com/gorilla/mux"
 	"go.uber.org/zap"
 	"golang.org/x/sys/unix"
+	"gopkg.in/yaml.v2"
 )
 
 var (
 	listenApi    = flag.String("listen-api", "[::]:7707", "Address and port (TCP) to listen for the manager API server")
+	confFile     = flag.String("config", "manager.yml", "Configuration file to read endpoints from")
 	buildVersion = "unknown"
 
 	mVersionInfo = prometheus.NewCounterVec(
@@ -30,6 +34,15 @@ var (
 )
 
 type managerServer struct {
+	mu  sync.RWMutex
+	cfg *config
+}
+
+type config struct {
+	Manager map[string]struct {
+		Username string
+		Password string
+	}
 }
 
 func (s *managerServer) StatusHandler(w http.ResponseWriter, r *http.Request) {
@@ -39,11 +52,28 @@ func (s *managerServer) StatusHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *managerServer) MachinesHandler(w http.ResponseWriter, r *http.Request) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
 	http.Error(w, "Not implemented", http.StatusNotImplemented)
 }
 
 func init() {
 	prometheus.MustRegister(mVersionInfo)
+}
+
+func (s *managerServer) loadConfig(file string) error {
+	cfgb, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+	var cfg config
+	if err := yaml.Unmarshal(cfgb, &cfg); err != nil {
+		return err
+	}
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.cfg = &cfg
+	return nil
 }
 
 func main() {
@@ -63,6 +93,10 @@ func main() {
 	rawlog.Info("vSphere deploy manager starting up")
 
 	srv := &managerServer{}
+	if err := srv.loadConfig(*confFile); err != nil {
+		rawlog.Fatal("Failed to load configuration file", zap.String("file", *confFile), zap.Error(err))
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", srv.StatusHandler).Methods("GET")
 	r.HandleFunc("/v1/machines", srv.MachinesHandler).Methods("GET")
@@ -70,7 +104,7 @@ func main() {
 
 	go func() {
 		if err := http.ListenAndServe(*listenApi, r); err != nil {
-			log.Fatalf("Failed to listen and serve status port: %v", err)
+			rawlog.Fatal("Failed to listen and serve status port", zap.Error(err))
 		}
 	}()
 
@@ -78,8 +112,19 @@ func main() {
 
 	// Wait for SIGINT / Ctrl+C
 	schan := make(chan os.Signal, 1)
-	signal.Notify(schan, unix.SIGINT)
-	<-schan
+	signal.Notify(schan, unix.SIGINT, unix.SIGHUP)
+	for {
+		s := <-schan
+		if s == unix.SIGHUP {
+			log.Infof("SIGHUP received! Reloading configuration")
+			if err := srv.loadConfig(*confFile); err != nil {
+				rawlog.Error("Failed to load new configuration file, old configuration is still active", zap.String("file", *confFile), zap.Error(err))
+			}
+		} else if s == unix.SIGINT {
+			break
+		}
+	}
+
 	daemon.SdNotify(false, daemon.SdNotifyStopping)
 	log.Infof("SIGINT received! Shutting down")
 
